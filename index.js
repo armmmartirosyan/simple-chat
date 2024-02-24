2b717b71e8a7e5e616d9b7c13e40942f60e0b79a
armmmartirosyann <137043444+armmmartirosyann@users.noreply.github.com>
2024-02-25 00:15:34 +0400

    added simple server for chat

---
+++ index.js
@@ -0,0 +1,89 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+const fs = require("fs");
+
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server, {
+  cors: {
+    origin: "*",
+    methods: ["GET", "POST"],
+  },
+});
+
+app.use(express.static("public"));
+
+io.on("connection", (socket) => {
+  const { username } = socket.handshake.query;
+
+  socket.join(`user_${username}`);
+
+  let clentsList = Array.from(io.sockets.sockets, ([_, value]) => value);
+  clentsList = clentsList.map((client) => client.handshake.query.username);
+  clentsList = clentsList.filter((client) => client !== username);
+
+  // To the client that just connected
+  io.to(`user_${username}`).emit("on_connect", clentsList);
+
+  // To the other clients
+  socket.broadcast.emit("new_user_connected", username);
+
+  console.log("A user connected - ", username);
+
+  // Handle new messages (to a friend)
+  socket.on("new_message", (message) => {
+    fs.readFile("./db.json", "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+      const messages = JSON.parse(data);
+      messages.push(message);
+
+      fs.writeFile("./db.json", JSON.stringify(messages, null, 2), (err) => {
+        if (err) {
+          console.error(err);
+          return;
+        }
+
+        if (message.to) {
+          io.to(`user_${message.to}`).emit("new_message", message);
+        }
+      });
+    });
+  });
+
+  // Load previous messages
+  socket.on("load_messages", (friendUsername) => {
+    // Send existing messages to the client
+    fs.readFile("./db.json", "utf8", (err, data) => {
+      if (err) {
+        console.error(err);
+        return;
+      }
+
+      const messages = JSON.parse(data);
+
+      const filteredMessages = messages.filter(
+        (message) =>
+          (message.from === friendUsername && message.to === username) ||
+          (message.from === username && message.to === friendUsername)
+      );
+
+      socket.emit("load_messages", filteredMessages);
+    });
+  });
+
+  // Handle disconnect
+  socket.on("disconnect", () => {
+    console.log("A user disconnected", "-", username);
+
+    socket.broadcast.emit("user_disconnected", username);
+  });
+});
+
+const PORT = process.env.PORT || 3004;
+server.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
